9ff90a18bc1891b2ffc273b3b05722702a36616b
Rustam Guseynov <rustam.guseynov@thomsonreuters.com>
2013-06-25 18:06:44 +0400

    Fixed bond addition; todo is that bond addition wouldn't show group name

--- YieldMap/Forms/PortfolioForm/BondSelectorForm.vb
+++ YieldMap/Forms/PortfolioForm/BondSelectorForm.vb
@@ -76,7 +76,7 @@ Namespace Forms.PortfolioForm
         End Sub
 
         Private Sub OkButtonClick(ByVal sender As Object, ByVal e As EventArgs) Handles OkButton.Click
-            _selectedRICs = (From aRow As DataGridViewRow In BondListDGV.SelectedRows Select CStr(aRow.Cells(0).Value)).ToList
+            _selectedRICs = (From aRow As DataGridViewRow In BondListDGV.SelectedRows Select CStr(aRow.Cells("RIC").Value)).ToList
         End Sub
 
         Private Sub IncludeCBCheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles IncludeCB.CheckedChanged
